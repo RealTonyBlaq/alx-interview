95f8d252534a1a677921dffea4cfd717abef9cba
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-12 23:06:00 +0100

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,7 +7,7 @@ def minOperations(n: int) -> int:
     Returns an integer
     If n is impossible to achieve, returns 0
     """
-    factors, ops = [2, 3, 5, 7, 11, 13], []
+    factors, ops = [2, 3, 5, 7, 11, 13, 17], []
     while n > 1:
         for i in factors:
             if n % i == 0:
