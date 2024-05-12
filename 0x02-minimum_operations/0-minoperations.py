5a27032a62e6029b59150561669ebadd19cd0002
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-12 22:51:15 +0100

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,7 +12,7 @@ def minOperations(n: int) -> int:
     Returns an integer
     If n is impossible to achieve, returns 0
     """
-    H, ops, copy = 1, 0, 0
+    H, ops, copy = 1, 0, 
     while H < n:
         if copy + H <= n:
             copy = H
