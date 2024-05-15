a29dfa847af46d93524fef95010fb0f9d10b5053
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-15 17:58:45 +0100

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,16 +8,19 @@ each box may contain keys to the other boxes.
 
 def canUnlockAll(boxes):
     """ Checks if all the boxes are unlocked """
-    n = len(boxes)
-    visited = [False] * n
+    box_size = len(boxes)
+    if box_size == 0:
+        return False
+
+    visited = [False] * box_size
     visited[0] = True
 
     queue = [0]
 
     while queue:
-        current_box = queue.pop(0)
-        for key in boxes[current_box]:
-            if key < n and not visited[key]:
+        current = queue.pop(0)
+        for key in boxes[current]:
+            if key < box_size and not visited[key]:
                 visited[key] = True
                 queue.append(key)
 
