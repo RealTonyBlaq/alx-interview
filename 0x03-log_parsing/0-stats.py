c03953be5606a04ffd0da3b8d3643260da121128
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-26 15:54:48 +0100

    parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,4 +5,6 @@ import sys
 import signal
 
 
-read_format = <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+read_format = '<IP Address> - [<date>] \"GET /projects/260 HTTP/1.1\" <status code> <file size>'
+
+print(read_format)
