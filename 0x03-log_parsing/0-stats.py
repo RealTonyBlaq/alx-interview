e09780314c5bc9b330a7e0b202f8343374e3e5ff
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-26 16:17:14 +0100

    parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,7 +8,8 @@ import signal
 def print_stats(stats):
     """ Prints stats """
     for code, count in stats.items():
-        if count
+        if count > 0:
+            print(f'{code}: {}')
 
 read_format = '<IP Address> - [<date>] \"GET /projects/260 HTTP/1.1\" <status code> <file size>'
 status_codes = {
