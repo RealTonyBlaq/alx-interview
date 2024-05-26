8b07155c21a69800581b4aa24e5a96e1619ace42
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-26 16:03:34 +0100

    parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,14 +6,15 @@ import signal
 
 
 read_format = '<IP Address> - [<date>] \"GET /projects/260 HTTP/1.1\" <status code> <file size>'
-status_codes = {'200': 0,
-                '301': 0,
-                '400': 0,
-                '401': 0,
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
                 '403': 0,
                 '404': 0,
                 '405': 0,
-                '500': 0
+                '500': 0,
     }
 
 with sys.stdin as f:
