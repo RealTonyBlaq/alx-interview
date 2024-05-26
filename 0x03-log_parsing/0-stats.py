d9fe43573a5e3a423f39c66f4ea3c028432cd6c4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-26 16:39:18 +0100

    parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -31,13 +31,12 @@ with sys.stdin as f:
     for line in f.readlines():
         new_line = line.split()
         total_size += f.tell()
-        if len(new_line) == 8:
-            code = new_line[-2]
-            if type(code) is int:
-                status_codes[str(code)] += 1
-            if page_number % 10:
-                try:
-                    print_stats(status_codes, total_size)
+        code = new_line[-2]
+        if type(code) is int:
+            status_codes[str(code)] += 1
+        if page_number % 10:
+            try:
+                print_stats(status_codes, total_size)
                 except KeyboardInterrupt:
                     print_stats(status_codes, total_size)
                 
