56247c9f7acd368bb70ac0211c1e4afbb9239470
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-03 17:43:33 +0100

    queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,19 +3,19 @@
 
 import sys
 
-
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-
-try:
-    N = int(sys.argv[1])
-    if N < 4:
-        print('N must be at least 4')
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
         sys.exit(1)
-except ValueError:
-    print('N must be a number')
-    sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        if N < 4:
+            print('N must be at least 4')
+            sys.exit(1)
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
 
 queen = []
 king = []
