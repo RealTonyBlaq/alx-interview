43a88fa02f1ad1913702f41c9510e2e34fb59359
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-03 17:49:44 +0100

    queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -36,21 +36,6 @@ def print_solutions(solutions):
             formatted_solution.append([row, col])
         print(formatted_solution)
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
 
 def main():
     if len(sys.argv) != 2:
