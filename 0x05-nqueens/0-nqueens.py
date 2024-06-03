b94f8f29131ddde53a7b92267afc426bf3040cdb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-03 17:42:50 +0100

    queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -12,10 +12,10 @@ try:
     N = int(sys.argv[1])
     if N < 4:
         print('N must be at least 4')
-        exit(1)
+        sys.exit(1)
 except ValueError:
     print('N must be a number')
-    exit(1)
+    sys.exit(1)
 
 queen = []
 king = []
