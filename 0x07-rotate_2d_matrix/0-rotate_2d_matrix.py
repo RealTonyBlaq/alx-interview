92d3f72a4145ddfc4e000d2c3d01c2ab8ab061cb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-13 21:20:22 +0100

    rotate a matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,9 +5,8 @@ def rotate_2d_matrix(matrix):
     """ Rotates an n x n Matrix 90 degrees clockwise """
     if matrix and type(matrix) is list:
         n = len(matrix)
-        new = [[]]
+        new = [[]] * n
         for i in range(n):
-            new[i] = []
             for matris in matrix:
                 new[i].append(matris[i])
             new[i].reverse()
