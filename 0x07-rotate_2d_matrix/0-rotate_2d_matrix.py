7728507d2e6d2824c8d9a8e95c98f8e09dbbe171
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-13 21:29:43 +0100

    rotate a matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,4 +5,5 @@ def rotate_2d_matrix(matrix):
     """ Rotates an n x n Matrix 90 degrees clockwise """
     n = len(matrix)
     for r in range(n):
-        for c in range
+        for c in range(n):
+            
