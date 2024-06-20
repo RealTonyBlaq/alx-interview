76124344aaf44cd8d606d4509f35c75394c3218a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-20 19:19:13 +0100

    fixed pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,16 +6,16 @@ def makeChange(coins, total):
     """ Making change """
     if total <= 0:
         return 0
-    
+
     # Initialize the DP table with "infinity" (a large number)
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
+
     # Populate the DP table
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-    
+
     # If dp[total] is still infinity, it means it's not possible
     # to make the total
     return dp[total] if dp[total] != float('inf') else -1
