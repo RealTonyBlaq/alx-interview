bc0eb31c6a8894cd4f87c225616ba0e5120db678
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-01 09:27:05 +0100

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -25,13 +25,13 @@ def island_perimeter(grid):
         for c in range(cols):
             if grid[r][c] == 1:
                 # Check all four directions
-                if r == 0 or grid[r-1][c] == 0:  # Up
+                if r == 0 or grid[r - 1][c] == 0:  # Up
                     perimeter += 1
-                if r == rows - 1 or grid[r+1][c] == 0:  # Down
+                if r == rows - 1 or grid[r + 1][c] == 0:  # Down
                     perimeter += 1
-                if c == 0 or grid[r][c-1] == 0:  # Left
+                if c == 0 or grid[r][c - 1] == 0:  # Left
                     perimeter += 1
-                if c == cols-1 or grid[r][c+1] == 0:  # Right
+                if c == cols - 1 or grid[r][c+1] == 0:  # Right
                     perimeter += 1
 
     return perimeter
