9a2aa6702fbc2d180e347a265fbd40b0c9d6aa4f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-08 21:17:21 +0100

    game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,58 +1,82 @@
 #!/usr/bin/python3
-""" Prime Game """
+"""Prime Game"""
 
 
 def isWinner(x, nums):
-    def sieve(max_n):
-        """
-        Generate a list of prime numbers up to max_n
-        using the Sieve of Eratosthenes
-        """
-        is_prime = [True] * (max_n + 1)
-        p = 2
-        while (p * p <= max_n):
-            if is_prime[p]:
-                for i in range(p * p, max_n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        primes = []
-        for p in range(2, max_n + 1):
-            if is_prime[p]:
-                primes.append(p)
-        return primes
+    """isWinner function
+    Args:
+        x  (int): the number of rounds
+        nums  (Array): An Array
+    Return: Name of the player that won the most rounds
+    """
+    if x <= 0 or not nums:
+        return None
 
-    # Determine the maximum value of n across all rounds
+    # Helper function to get all primes up to max_num
+    # using Sieve of Eratosthenes
+    def sieve(max_num):
+        """Sieve of Erastosthenes
+        Args:
+            max_nums  (int): maximum number
+        """
+        is_prime = [True] * (max_num + 1)
+        p = 2
+        while (p * p <= max_num):
+            if is_prime[p]:
+                for i in range(p * p, max_num + 1, p):
+                    if is_prime[p]:
+                        for i in range(p * p, max_num + 1, p):
+                            is_prime[i] = False
+            p += 1
+        prime_nums = [p for p in range(2, max_num + 1) if is_prime[p]]
+        return prime_nums
+
+    # Get the maximun number in nums to limit the sieve range
     max_n = max(nums)
     primes = sieve(max_n)
 
-    # Results for Maria and Ben
+    # Function to simulate the game for a single round
+    def play_game(n):
+        if n < 2:
+            return 'Ben'  # Maria can not make a move
+
+        current_nums = list(range(1, n + 1))
+        move = 0  # for Maria, 1 for Ben
+
+        while True:
+            prime_found = False
+            for prime in primes:
+                if prime > n:
+                    break
+                if prime in current_nums:
+                    prime_found = True
+                    current_nums = [
+                            num for num in current_nums if num % prime != 0
+                            ]
+                    break
+
+            if not prime_found:
+                break
+
+            move = 1 - move
+
+        return 'Ben' if move == 0 else 'Maria'
+
+    # Play each game and count wins
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        # Create a list to keep track of the current set
-        current_set = [True] * (n + 1)
-        prime_count = 0
-
-        for prime in primes:
-            if prime > n:
-                break
-            if current_set[prime]:
-                # Maria picks this prime and removes multiples
-                prime_count += 1
-                for multiple in range(prime, n + 1, prime):
-                    current_set[multiple] = False
-
-        # Determine the winner for this round
-        if prime_count % 2 == 1:
+        winner = play_game(n)
+        if winner == 'Maria':
             maria_wins += 1
         else:
             ben_wins += 1
 
-    # Determine the overall winner
+    # Determine the overal winner
     if maria_wins > ben_wins:
-        return "Maria"
+        return 'Maria'
     elif ben_wins > maria_wins:
-        return "Ben"
+        return 'Ben'
     else:
         return None
